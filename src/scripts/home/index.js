58a1023fa5c11d0b87cf0f65197c4e7593ad69b8
Romario Negreiros <nromario482@gmail.com>
2022-04-14 18:21:09 -0300

    feat: add redirect functionality in explore button of home page + create redirect function

--- src/scripts/home/index.js
+++ src/scripts/home/index.js
@@ -1,3 +1,8 @@
 import addMenuFunctionality from '../common/AddMenuFunctionality.js'
+import redirect from '../common/redirect.js'
 
 addMenuFunctionality()
+
+const explorebtn = document.querySelector('.explore__btn')
+
+explorebtn.addEventListener('click', () => redirect('destinations.html'))
