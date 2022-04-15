1a9677afb297ab8d08b8ebd430e37c90c253d524
Romario Negreiros <nromario482@gmail.com>
2022-04-15 11:20:23 -0300

    feat: add active class to body tag

--- src/scripts/common/MenuHandler.js
+++ src/scripts/common/MenuHandler.js
@@ -3,12 +3,14 @@ class MenuHandler {
     document.querySelector('.nav__mobile-open').classList.add('nav__mobile-open--inactive')
     document.querySelector('.nav__mobile-close').classList.add('nav__mobile-close--active')
     document.querySelector('.nav__list').classList.add('nav__list--active')
+    document.querySelector('body').classList.add('body--active')
   }
 
   close() {
     document.querySelector('.nav__mobile-open').classList.remove('nav__mobile-open--inactive')
     document.querySelector('.nav__mobile-close').classList.remove('nav__mobile-close--active')
     document.querySelector('.nav__list').classList.remove('nav__list--active')
+    document.querySelector('body').classList.remove('body--active')
   }
 }
 
