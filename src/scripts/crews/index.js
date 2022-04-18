6068f04aea16809daaf5088d396b2bf81eb515b5
Romario Negreiros <nromario482@gmail.com>
2022-04-18 11:45:04 -0300

    feat: add content to page when it loads

--- src/scripts/crews/index.js
+++ src/scripts/crews/index.js
@@ -1,3 +1,22 @@
 import addMenuFunctionality from '../common/AddMenuFunctionality.js'
+import redirectWithQueryStrings from '../common/redirectWithQueryStrings.js'
+import onLoadHandler from '../common/onLoadHandler.js'
+
+const main = document.querySelector('.main-inative')
+const loader = document.querySelector('.loader')
+const options = Array.from(document.querySelector('.content__options').children)
+
+function manageClasses(index) {
+  options[index].classList.add('active-option')
+  main.classList.remove('main-inative')
+  loader.classList.add('loader-inative')
+}
+
+options.forEach((option, index) =>
+  option.addEventListener('click', () => redirectWithQueryStrings('crews.html', index))
+)
+
+window.addEventListener('load', () => onLoadHandler('crew', manageClasses))
 
 addMenuFunctionality()
+
