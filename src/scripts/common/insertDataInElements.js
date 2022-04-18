50751b027ca03b420b9511a87f6781a41d93f31c
Romario Negreiros <nromario482@gmail.com>
2022-04-18 11:46:25 -0300

    feat: add condition to check if there are portrait and landscape images and handle it

---
+++ src/scripts/common/insertDataInElements.js
@@ -0,0 +1,26 @@
+function insertDataInElements(data) {
+  for (const key in data) {
+    if (key === 'images') {
+      if (data[key].portrait && data[key].landscape) {
+        const portraitImg = document.querySelector('.portrait-image')
+        portraitImg.setAttribute('src', data[key].portrait)
+        portraitImg.setAttribute('alt', data.name)
+
+        const landscapeImg = document.querySelector('.landscape-image')
+        landscapeImg.setAttribute('src', data[key].landscape)
+        landscapeImg.setAttribute('alt', data.name)
+      } else {
+        const image = document.querySelector('.images')
+
+        image.setAttribute('src', data[key].webp)
+        image.setAttribute('alt', data.name)
+      }
+      continue
+    }
+    const element = document.querySelector(`.${key}`)
+    console.log(data, data[key])
+    element.textContent = data[key]
+  }
+}
+
+export default insertDataInElements
